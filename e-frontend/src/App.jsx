9ee453e8cb64bda75b393902751b99d86e1d16b7
bala guru <duskcodertec@gmail.com>
2023-12-28 11:05:57 +0530

    bala

--- e-frontend/src/App.jsx
+++ e-frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import { useState } from 'react';
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
 import './App.css'
 import Layout from './Component/Layout';
+import Home from './Pages/Home';
+import SingleProduct from './Pages/singleProduct';
 
 function App() {
-  
+
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Layout/>}>
-      
-      </Route>
-    </Routes>
-    
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/singleProduct' element={<SingleProduct />} />
+        </Route>
+      </Routes>
+
     </>
   )
 }
