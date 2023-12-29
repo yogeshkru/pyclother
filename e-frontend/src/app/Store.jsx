f0b393d5d507e21577e928f0c9a7038d249d66e2
duskcoder <duskcodertec@gmail.com>
2023-12-29 10:35:28 +0530

    yogesh

--- e-frontend/src/app/Store.jsx
+++ e-frontend/src/app/Store.jsx
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import {configureStore} from "@reduxjs/toolkit";
 
 export const store = configureStore({
-    reducer:{}
-})  
-=======
-import {configureStore} from '@reduxjs/toolkit';
-
-
-export const store=configureStore({
     reducer:{
-
+        
     }
-})
->>>>>>> c2b3be7ecb3ab7280f8ce41a18fe385a46a081e0
+})  
