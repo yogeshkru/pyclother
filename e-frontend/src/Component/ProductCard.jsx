b9a06d0af8bac34541369901746c8b3a7de32afe
gokulkrishnan1509 <gokulkrishnan1509@gmail.com>
2024-01-02 14:41:45 +0530

    new change gokul

--- e-frontend/src/Component/ProductCard.jsx
+++ e-frontend/src/Component/ProductCard.jsx
@@ -122,7 +122,431 @@ const ProductCard = function () {
         </div>
       </div>
       
-      
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={white} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={woodie} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={tshirt} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={yellow} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
+      <div className="col-xl-3 col-lg-4 col-md-6 mt-3 ">
+        <div className="productCard position-relative  ">
+          <div className="productCard-wishlist-icon position-absolute">
+            <Link>
+             
+              <CiHeart className="productCard-wistlist-img fs-4" />
+
+            </Link>
+          </div>
+          <div className="box ">
+
+            
+            <img src={blue} alt="t-shirt" className="ms-2 image-fluid productCard-image d-flex mx-auto" />
+
+          </div>
+          <div className="product-details">
+            <h6 className="productCard-brand mt-3 mb-0 ms-3">H&M</h6>
+            <p className="productCard-title mt-2 mb-0 ms-3">
+              Men's Cotton Pure cotton T-S
+            </p>
+
+            <h6 className="productCard-price mt-2 ms-3">Rs-888</h6>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
