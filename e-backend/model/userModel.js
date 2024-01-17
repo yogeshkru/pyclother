9094795d172c2cfbf222f42f35cfa35ae097c24a
duskcoder <duskcodertec@gmail.com>
2024-01-17 14:51:33 +0530

    yogesh

--- e-backend/model/userModel.js
+++ e-backend/model/userModel.js
@@ -1,39 +1,62 @@
-const mongoose=require('mongoose')
-const bcrypt=require('bcrypt')
-const crypto=require('crypto')
-const userSchema=new mongoose.Schema(
-    {
-        name:{
-            type:String,
-            required:[true,'name is required'],
-            trim:true
-        },
-        email:{
-            type:String,
-            required:[true,"Email is required"],
-         
-        },
-        phone:{
-            type:Number,
-            required:[true,"Phone is required"]
-        },
-        password:{
-            type:String,
-            required:[true,"Password is required"],
-            select:false
-
-        },
-        wishlist:[{type:mongoose.Schema.Types.ObjectId,ref:"Product"}],
-        passwordChangedAt:Date,
-        passwordResetToken:String,
-        passwordResetTokenExpired:Date
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+      trim: true,
     },
-    {
-        timestamps:true
-    }
-)
-userSchema.pre("save",async function (next){
-    if(!this.isModified('password')) return next();
-    this.password=await bcrypt.hash(this.password,14)
-})
-module.exports=mongoose.model('User',userSchema)
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    phone: {
+      type: Number,
+      required: [true, "Phone is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+    },
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetTokenExpired: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 14);
+});
+userSchema.methods.comparePasswordInDb = async function (pwd, pswDB) {
+  return await bcrypt.compare(pwd, pswDB);
+};
+
+userSchema.methods.isPasswordChange = async function (jwttoken) {
+  if (this.passwordChangedAt) {
+    const passwordChangedtimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return jwttoken < passwordChangedtimestamp;
+  } else {
+    return false;
+  }
+};
+
+userSchema.methods.createResetPasswordToken = async function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.passwordResetTokenExpired = Date.now() + 10 * 60 * 1000;
+  return resetToken;
+};
+module.exports = mongoose.model("User", userSchema);
