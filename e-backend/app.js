9ff23bed43bca58e885acc8410e0366546736672
gokulkrishnan1509 <gokulkrishnan1509@gmail.com>
2024-01-18 19:07:47 +0530

    api end-point for admin

--- e-backend/app.js
+++ e-backend/app.js
@@ -1,33 +1,43 @@
-const express=require('express');
-const app=express()
-const cors=require('cors')
-const bodyparser=require('body-parser')
-const cookie=require('cookie-parser')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const cookie = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
 
-const globalError=require('./utils/globalErrorhandler')
-const customError=require('./utils/customError')
-
+const globalError = require("./utils/globalErrorhandler");
+const customError = require("./utils/customError");
 
 // *****************Third part liberary****************
 
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended:true}))
-app.use(cookie())
-app.use(cors())
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cookie());
+app.use(cors());
+let limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message:
+    "We have received too many request from this UP. Please try after one hour",
+});
 
-if(process.env.NODE_ENV !== "PRODUCTION"){
-    require("dotenv").config()
+app.use("/api", limiter);
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config();
 }
 
+require("./routes/userRoute")(app);
+require("./routes/adminUserRoutes")(app);
 
-require("./routes/userRoute")(app)
 //Routes error handler
-app.all("*",(req,res,next)=>{
-    const error=new customError(
-        `can't find ${req.originalUrl} on the server`,404
-    )
-    next(error)
-})
+app.all("*", (req, res, next) => {
+  const error = new customError(
+    `can't find ${req.originalUrl} on the server`,
+    404
+  );
+  next(error);
+});
 
-app.use(globalError)
-module.exports=app
+app.use(globalError);
+module.exports = app;
