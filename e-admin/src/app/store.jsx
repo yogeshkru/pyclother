0637e3ea04d2e22393fd5bb398b91f0ef2550471
duskcoder <duskcodertec@gmail.com>
2024-01-29 11:35:43 +0530

    yogesh

--- e-admin/src/app/store.jsx
+++ e-admin/src/app/store.jsx
@@ -1,12 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import brandDetails  from "../features/brandSlice";
-
+import categorySlice from "../features/category/categorySlice";
 import couponSlice from "../features/coupon/couponSlice";
 
 export const store = configureStore({
     reducer:{
         brand:brandDetails,
-        
+        category:categorySlice,
         coupon:couponSlice
     }
 })
