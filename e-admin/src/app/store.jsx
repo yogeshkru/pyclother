d021ac770081d175f2495749f23c3a15618768a6
duskcoder <duskcodertec@gmail.com>
2024-01-29 14:14:29 +0530

    gitpull

--- e-admin/src/app/store.jsx
+++ e-admin/src/app/store.jsx
@@ -4,13 +4,6 @@ import couponSlice from "../features/coupon/couponSlice";
 import productSlice from "../features/product/productSlice";
 import superAdminSlice from "../features/SuperAdmin/superAdminSlice";
 export const store = configureStore({
-<<<<<<< HEAD
-    reducer:{
-        brand:brandDetails,
-        // category:categorySlice
-    }
-})
-=======
   reducer: {
     brand: brandDetails,
     coupon: couponSlice,
@@ -18,4 +11,3 @@ export const store = configureStore({
     superadmin: superAdminSlice,
   },
 });
->>>>>>> 1d011f8e6951f6812d2fe36031bb4e141fc5caf9
