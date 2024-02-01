c02c4e739696f851380e49f4e4e82e83c9dddd98
duskcoder <duskcodertec@gmail.com>
2024-02-01 10:07:13 +0530

    yogesh

--- e-admin/src/pages/Addproduct.jsx
+++ e-admin/src/pages/Addproduct.jsx
@@ -4,6 +4,7 @@ import "react-quill/dist/quill.snow.css";
 import { InboxOutlined } from "@ant-design/icons";
 import { message, Upload } from "antd";
 import UseInput from "../useCustom/useInput";
+
 const { Dragger } = Upload;
 
 const props = {
@@ -34,15 +35,231 @@ function Addproduct() {
   return (
     <div className="row">
       <div className="col-lg-12">
-        <div className="text-center">
-          <h3>Product Details</h3>
-          <div className="row justify-content-center">
-            <div className="col-lg-5">
-              <form>
-                <div className="mt-3 mb-2">
-                <UseInput type="text"  />
+        <div className="row">
+          <div className="col-lg-8">
+            <div class="card">
+              <div class="card-body product_input">
+                <div className="row mb-3">
+                  <div className="col-lg-4">
+                    <label className="fw-bold fs-10">Product Name</label>
+                    <UseInput type="text" label="Name" />
+                  </div>
+                  <div className="col-lg-4">
+                    <label className="fw-bold fs-10">Product Model</label>
+                    <UseInput type="text" label="Model" />
+                  </div>
+
+                  <div className="col-lg-4">
+                    <label className="fw-bold fs-10">SKU</label>
+                    <UseInput type="text" label="SKU" />
+                  </div>
                 </div>
-              </form>
+                <div className="row mb-2  ">
+                  <div className="col-lg-4">
+                    <label className="fw-bold fs-10">Stack</label>
+                    <UseInput type="text" label="Stack" />
+                  </div>
+                  <div className="col-lg-4">
+                    <label className="fw-bold fs-10">Product Price</label>
+                    <UseInput type="text" label="Model" />
+                  </div>
+
+                  <div className="col-lg-4">
+                    <div className="">
+                      <label className="fw-bold fs-10">Tax</label>
+                      <select
+                        class="form-select"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="row mb-2">
+                  <div className="col-lg-4">
+                    <div className="">
+                      <label className="fw-bold fs-10">Brand</label>
+                      <select
+                        class="form-select"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="col-lg-4">
+                    <div className="">
+                      <label className="fw-bold fs-10">Color</label>
+                      <select
+                        class="form-select"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-4">
+                    <div className="">
+                      <label className="fw-bold fs-10">Category</label>
+                      <select
+                        class="form-select"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-lg-4">
+                    <div className="">
+                      <label className="fw-bold fs-10">Reward Point</label>
+                      <UseInput type="text" label="Reward Point" />
+                    </div>
+                  </div>
+                  <div className="col-lg-4">
+                    <div className="">
+                      <label className="fw-bold fs-10">Status</label>
+                      <select
+                        class="form-select"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-4">
+                    <div className="">
+                      <label className="fw-bold fs-10">Sort</label>
+                      <select
+                        class="form-select"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-3 mb-2">
+                  <label className="fw-bold fs-10">Description</label>
+                  <UseInput type="text" label="Description" />
+                </div>
+
+                <div className="mt-3 mb-2">
+                  <label className="fw-bold fs-10">Image</label>
+                  <select
+                    class="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4">
+            <div class="card">
+              <div class="card-body product_input">
+                <div className="">
+                  <h4>Dimension</h4>
+                </div>
+
+                <div className="row mb-1">
+                  <div className="col-lg-6">
+                    <label className="fw-bold fs-10">Length</label>
+                    <UseInput type="text" label="Length" />
+                  </div>
+                  <div className="col-lg-6">
+                    <label className="fw-bold fs-10">Weight</label>
+                    <UseInput type="text" label="Weight" />
+                  </div>
+                </div>
+                <div className="mt-3 mb-2">
+                  <div className="row">
+                    <div className="col-lg-6">
+                      <label className="fw-bold fs-10">Height</label>
+                      <UseInput type="text" label="Length" />
+                    </div>
+                    <div className="col-lg-6">
+                      <label className="fw-bold fs-10">Dimension class</label>
+                      <select
+                        class="form-select"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-3 mb-2">
+                  <div className="row">
+                    <div className="col-lg-6">
+                      <label className="fw-bold fs-10">Weight</label>
+                      <UseInput type="text" label="Length" />
+                    </div>
+                    <div className="col-lg-6">
+                      <label className="fw-bold fs-10">Weight class</label>
+                      <select
+                        class="form-select"
+                        aria-label="Default select example"
+                      >
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mt-1">
+              <div class="card-body  product_input">
+                <div className="">
+                  <h4>SEO</h4>
+                </div>
+                <div className="mb-2">
+                  <label className="fw-bold fs-10">Meta Title </label>
+                  <UseInput type="text" label="Meta Title" />
+                </div>
+                <div className="mb-2">
+                  <label className="fw-bold fs-10">Meta Description </label>
+                  <UseInput type="text" label="Meta Description" />
+                </div>
+                <div className="mb-2">
+                  <label className="fw-bold fs-10">Meta Keyword </label>
+                  <UseInput type="text" label="Meta Keyword" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
