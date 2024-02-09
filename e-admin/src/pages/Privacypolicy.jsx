134c7783c317d466c9ab0d0b3c3b331b5e26a579
VCodeWonders <vcodewo@gmail.com>
2024-02-09 10:49:27 +0530

    yogesh

--- e-admin/src/pages/Privacypolicy.jsx
+++ e-admin/src/pages/Privacypolicy.jsx
@@ -1,25 +1,36 @@
 import React, { useState } from "react";
-import ReactQuill from "react-quill";
+
 import "../styles/Mainlayout.css";
-import "react-quill/dist/quill.snow.css";
-import {privacy} from "../features/Privacypolicy/Privarypolicyslice";
+
+import { privacy } from "../features/Privacypolicy/Privarypolicyslice";
 import { useDispatch } from "react-redux";
 function Privacypolicy() {
-    const dispatch=useDispatch()
+  const dispatch = useDispatch();
   const [value, setValue] = useState("");
-  
-  let privacyPolicy={privary_policy:value}
- 
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-     dispatch(privacy(privacyPolicy))
-   
-  }
+
+  let privacyPolicy = { privary_policy: value };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(privacy(privacyPolicy));
+    setValue("")
+  };
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <ReactQuill theme="snow" value={value} onChange={setValue} />;
-        <button type="submit"   className="brand_padding--border">Submit</button>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Example textarea</label>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            value={value}
+            onChange={(e)=>setValue(e.target.value)}
+          ></textarea>
+        </div>
+        <button type="submit" className="brand_padding--border">
+          Submit
+        </button>
       </form>
     </div>
   );
